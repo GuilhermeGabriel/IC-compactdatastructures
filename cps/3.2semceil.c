670e6568ecefd25cb50e31337dfab4580d1a99d1
GuilhermeGabriel <guilhermegabriel90@gmail.com>
2021-03-04 14:54:16 -0300

    add version without ceil

--- cps/3.2semceil.c
+++ cps/3.2semceil.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-#define Ceil(x,y) (x/y + (x%y!=0))
-
 unsigned int n=10,l=5,w=32,W[2];
 
 int pot(int b,int ex){
@@ -11,25 +9,25 @@ int pot(int b,int ex){
 
 int bitRead(unsigned int *W, int j){
 	int r=((j-1)%w)+1;
-	return (W[Ceil(j,w)]/pot(2,w-r))%2;
+	return (W[j/w]/pot(2,w-r))%2;
 }
 
 void bitSet(unsigned int *W, int j){
 	int r=((j-1)%w)+1;
-	if((W[Ceil(j,w)]/pot(2,w-r)%2)==0){
-		W[Ceil(j,w)]+=pot(2,w-r);
+	if((W[j/w]/pot(2,w-r)%2)==0){
+		W[j/w]+=pot(2,w-r);
 	}
 }
 
 void bitClear(int *W, int j){
 	int r=((j-1)%w)+1;
-	if((W[Ceil(j,w)]/pot(2,w-r)%2)==1){
-		W[Ceil(j,w)]-=pot(2,w-r);
+	if((W[(j/w)]/pot(2,w-r)%2)==1){
+		W[(j/w)]-=pot(2,w-r);
 	}
 }
 
 int main(){
-	for (int bit =0; bit < 64; bit++) {
+  for (int bit =0; bit < 64; bit++) {
     bitSet(W,bit);
     for(int i=0;i<32;i++)
       printf("%d", bitRead(W,i));
@@ -39,6 +37,6 @@ int main(){
   }
 
   printf("\n\n %u %u", W[0], W[1]);
-
-	return 0;
+  
+  return 0;
 }
